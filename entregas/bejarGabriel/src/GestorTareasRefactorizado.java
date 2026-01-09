4d1890c78ab366f5dfd1af1062a07a0386d55502
Gabriel Béjar <gabriel.bejar@alumnos.uneatlantico.es>
2026-01-09 15:19:55 +0100

    Agregamos el boolean que habia antes con la misma funcion, identificar si ya esta completada o no una tarea. (decidi incluir el "Gestor de tareas v2.0" que habia antes) Tambien coloco un while que dara funcion a la eleccion de lo que decida el usuario.

--- entregas/bejarGabriel/src/GestorTareasRefactorizado.java
+++ entregas/bejarGabriel/src/GestorTareasRefactorizado.java
@@ -9,4 +9,36 @@ public class GestorTareasRefactorizado {
     private static boolean[] tareasCompletadas = new boolean[CAPACIDAD_MAXIMA];
     
     private static int cantidadTareas = 0;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean continuar = true;
+
+        System.out.println("=== Gestor de Tareas v2.0==");
+
+        while (continuar) {
+            mostrarMenu();
+            int opcion = leerEntero("Seleccione una opción: ");
+
+            if (opcion == 1) {
+                anadirTarea();
+            } else if (opcion == 2) {
+                marcarTareaCompletada();
+            } else if (opcion == 3) {
+                verTareasPendientes();
+            } else if (opcion == 4) {
+                mostrarEstadisticas();
+            } else if (opcion == 5) {
+                System.out.println("Cerrando aplicación...");
+                continuar = false;
+            } else {
+                System.out.println("Opción no válida. Intente de nuevo.");
+            }
+            
+            System.out.println(); 
+        }
+        
+        scanner.close();
+    }
+
 }
